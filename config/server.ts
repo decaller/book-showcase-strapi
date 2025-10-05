1da0eec9996da29b4795a7818e9cbde7434dafd7
Harridi Ilman Tovid <harridiilmantovid@gmail.com>
2025-10-05 19:38:25 +0700

    change the server.ts to put the url directly

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL', 'http://localhost:1337'),
+  url: env('URL', 'https://admin.insantaqwa.org/karya-smp'),
   app: {
     keys: env.array('APP_KEYS'),
   },
