9a416d5a73064bb65357afb9c8743ed89823d2e5
Harridi Ilman Tovid <harridiilmantovid@gmail.com>
2025-10-05 19:07:40 +0700

    adjusting middleware security

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,23 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  // Replace 'strapi::security' string with this object
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          // This 'unsafe-inline' is the key to fixing the error
+          'script-src': ["'self'", "'unsafe-inline'"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
