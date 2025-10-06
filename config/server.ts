bde2e9cd1bba856a0d23e01379a658cef099fe6f
Harridi Ilman Tovid <harridiilmantovid@gmail.com>
2025-10-06 12:23:16 +0700

    repair server.ts to add proxy

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,7 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('URL', 'https://admin.insantaqwa.org/'),
+  proxy:true,
   app: {
     keys: env.array('APP_KEYS'),
   },
