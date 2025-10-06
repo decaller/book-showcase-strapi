a29a7c04c9d5ccd2e7e772e0b05af2dad9cc8580
Harridi Ilman Tovid <harridiilmantovid@gmail.com>
2025-10-06 12:28:33 +0700

    server ts proxy koa

--- config/server.ts
+++ config/server.ts
@@ -2,7 +2,7 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('URL', 'https://admin.insantaqwa.org/'),
-  proxy:true,
+  proxy: { koa: env.bool('IS_PROXIED', true) },
   app: {
     keys: env.array('APP_KEYS'),
   },
