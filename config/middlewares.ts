ce8998062c975fba8e973c138bd1bba3520f4657
Harridi Ilman Tovid <harridiilmantovid@gmail.com>
2025-10-06 11:17:51 +0700

    updateing the middlewear

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,23 +1,26 @@
-export default [
-
-  'strapi::logger',
-
+// ./config/middlewares.js
+module.exports = [
   'strapi::errors',
-
-  'strapi::security',
-
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
-
   'strapi::poweredBy',
-
+  'strapi::logger',
   'strapi::query',
-
   'strapi::body',
-
   'strapi::session',
-
   'strapi::favicon',
-
   'strapi::public',
-
-]; 
+];
